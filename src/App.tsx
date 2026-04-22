e34928dc465292b70bd8dd3826033dc34d3897b0
Liz Assistente <93327355+nowpublicidade@users.noreply.github.com>
2026-04-22 14:02:08 -0300

    Ajusta basename do BrowserRouter para domínio próprio

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/idealradios-main">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/ideal-eletric-way" element={<IdealElectricWay />} />
